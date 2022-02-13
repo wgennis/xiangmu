f598f1dfef2495ee42d2db60a0ae9a8feb8d22fa
wgennis <91460230+wgennis@users.noreply.github.com>
2022-02-14 03:05:51 +0800

    Update main.c

--- week1/s1/main.c
+++ week1/s1/main.c
@@ -1,1 +1,35 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] ans=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                ans[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                    ans[i][j]=Math.min(ans[i-1][j-1],Math.min(ans[i-1][j],ans[i][j-1]))+1;
+                }
+            }
+        }
+        int max=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                max=Math.max(max,ans[i][j]);
+            }
+        }
+        return max*max;
+
+    }
+}
 
